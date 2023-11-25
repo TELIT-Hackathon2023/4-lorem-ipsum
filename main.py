ba7e9cc629ba1d3937b0bbb54e8b33ea9e578766
Artem Naumenko <artem.naumenko@student.tuke.sk>
2023-11-25 18:45:30 +0100

    ConversationObject.py

--- main.py
+++ main.py
@@ -2,11 +2,19 @@ import os
 import openai
 import streamlit as st
 from objects.ConversationObject import ConversationObject
+from dotenv import load_dotenv
+import time
+
+load_dotenv()
+
+print("------------------")
+print(os.getenv("OPENAI_API_KEY"))
+print("------------------")
 
 conversations_list = [
-    ConversationObject("Title 1", [("Question 1", "Answer 1"), ("Question 2", "Answer 2")]),
-    ConversationObject("Title 2", [("Question 1", "Answer 1"),]),
-    ConversationObject("Title 3", [("Question 1", "Answer 1"), ("Question 2", "Answer 2"), ("Question 3", "Answer 3"), ("Question 4", "Answer 4")]),
+    ConversationObject("Title 1", [("Question 1", "Answer 1"), ("Question 2", "Answer 2")], time.time()),
+    ConversationObject("Title 2", [("Question 1", "Answer 1"),], time.time()),
+    ConversationObject("Title 3", [("Question 1", "Answer 1"), ("Question 2", "Answer 2"), ("Question 3", "Answer 3"), ("Question 4", "Answer 4")], time.time()),
 ]
 
 actual_conversation: ConversationObject = conversations_list[0]
