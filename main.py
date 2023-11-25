25f3205aeceed84f9cf1cc3d7c88c7fb29fc631b
Oleksandr Smahlii <92263887+alastar13@users.noreply.github.com>
2023-11-25 17:56:09 +0100

    The project's frontend skeleton

---
+++ main.py
@@ -0,0 +1,30 @@
+import openai
+import streamlit as st
+from secret import api_key
+
+
+def main():
+    st.title("Chatbot")
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input():
+        openai.api_key = api_key
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        response = openai.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                st.session_state.messages[-1]
+            ],
+        )
+        msg = response.choices[0].message
+        st.session_state.messages.append(msg)
+        st.chat_message("assistant").write(msg.content)
+
+
+if __name__ == "__main__":
+    main()
