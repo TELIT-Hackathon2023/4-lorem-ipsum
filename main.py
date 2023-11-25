bf6911a2b19c21ae69a191699f6003d89c268d1a
alastarXIII <alastarquinn@duck.com>
2023-11-25 18:11:18 +0100

    The switch-tab ver.

--- main.py
+++ main.py
@@ -1,38 +1,6 @@
-# import openai
-# import streamlit as st
-# from secret import api_key
-#
-#
-# def main():
-#     st.title("Chatbot")
-#     if "messages" not in st.session_state:
-#         st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-#
-#     for msg in st.session_state.messages:
-#         st.chat_message(msg["role"]).write(msg["content"])
-#
-#     if prompt := st.chat_input():
-#         openai.api_key = api_key
-#         st.session_state.messages.append({"role": "user", "content": prompt})
-#         st.chat_message("user").write(prompt)
-#         response = openai.chat.completions.create(
-#             model="gpt-4",
-#             messages=[
-#                 st.session_state.messages[-1]
-#             ],
-#         )
-#         msg = response.choices[0].message
-#         st.session_state.messages.append(msg)
-#         st.chat_message("assistant").write(msg.content)
-#
-#
-# if __name__ == "__main__":
-#     main()
-
-
-
 import openai
 import streamlit as st
+from secret import api_key
 from objects.ConversationObject import ConversationObject
 
 object_list = [
@@ -59,29 +27,25 @@ def main():
 
     st.title(actual_conversation.title)
     st.write(actual_conversation.content)
-    # if "messages" not in st.session_state:
-    #     st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
-    #
-    # for msg in st.session_state.messages:
-    #     st.chat_message(msg["role"]).write(msg["content"])
-    #
-    # if prompt := st.chat_input():
-    #     if not openai_api_key:
-    #         st.info("Please add your OpenAI API key to continue.")
-    #         st.stop()
-    #
-    #     openai.api_key = openai_api_key
-    #     st.session_state.messages.append({"role": "user", "content": prompt})
-    #     st.chat_message("user").write(prompt)
-    #     response = openai.chat.completions.create(
-    #         model="gpt-4-turbo",
-    #         messages=[
-    #             st.session_state.messages[-1]
-    #         ],
-    #     )
-    #     msg = response.choices[0].message
-    #     st.session_state.messages.append(msg)
-    #     st.chat_message("assistant").write(msg.content)
+    if "messages" not in st.session_state:
+        st.session_state["messages"] = [{"role": "assistant", "content": "How can I help you?"}]
+
+    for msg in st.session_state.messages:
+        st.chat_message(msg["role"]).write(msg["content"])
+
+    if prompt := st.chat_input():
+        openai.api_key = api_key
+        st.session_state.messages.append({"role": "user", "content": prompt})
+        st.chat_message("user").write(prompt)
+        response = openai.chat.completions.create(
+            model="gpt-4",
+            messages=[
+                st.session_state.messages[-1]
+            ],
+        )
+        msg = response.choices[0].message
+        st.session_state.messages.append(msg)
+        st.chat_message("assistant").write(msg.content)
 
 
 if __name__ == "__main__":
